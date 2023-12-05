bbe6c62ff6adef274c254850e4fdf99512c7fa86
Luis Mario Cabrera Torres <104865952+MarioCab07@users.noreply.github.com>
2023-12-04 19:33:01 -0600

    Update index.js

--- proyecto-19-webservice/index.js
+++ proyecto-19-webservice/index.js
@@ -1,29 +1,35 @@
 require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const express =  require('express');
-const debug = require('debug')('clinica-api:server');
-const mongoose =require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
 
 const envconfig = require('./src/config/env.config');
 const database = require('./src/config/db.config');
 const mainRouter =require('./src/routes/main.router');
 const {errorHandler }= require('./src/middleware/error.middleware')
 
+const CLIENT_URL = process.env.CLIENT_URL;
 const app = express();
-database.connect();
+
+database.connect();\
+
+app.use(logger("dev"));
 
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+
+//Static routes
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use('/api/v1', mainRouter);
 
 app.use(errorHandler);
 
-const port = envconfig.PORT;
-app.listen(port, () => {
-    debug(`Server is running on port ${port}`)
-  })
-
+app.use("/api/v1", apiRouter);
+module.exports = app;
