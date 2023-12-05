4b0231b406455cf2a1145a614f4377e7c4e01e71
Vanedrn03 <00025822@uca.edu.sv>
2023-12-05 09:22:01 -0600

    bug

--- proyecto-19-webservice/src/routes/main.router.js
+++ proyecto-19-webservice/src/routes/main.router.js
@@ -3,12 +3,10 @@ const router = express.Router();
 
 const citasRouter = require("./cita.router");
 const authRouter = require("./auth.router");
-const doctorRouter = require("./doctor.router");
 const antecedentesRouter = require("./antecedentes.router");
 
 router.use("/citas", citasRouter);
 router.use("/auth", authRouter);
-router.use("/doctor", doctorRouter);
 router.use("/antecedentes", antecedentesRouter);
 
 module.exports = router;
