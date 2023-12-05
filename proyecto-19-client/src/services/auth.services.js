6cb620c174c85e9e0acca664a6cec6d42e1bd7a2
FabrizioCalderon <00034022@uca.edu.sv>
2023-12-05 01:42:42 -0600

    errorsolucion

--- proyecto-19-client/src/services/auth.services.js
+++ proyecto-19-client/src/services/auth.services.js
@@ -37,9 +37,6 @@ export const registerUser = async (formData) => {
 };
 
 
-import axios from "axios";
-
-axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api/v1`;
 
 export const loginDoctor = async (formData) => {
   try {
