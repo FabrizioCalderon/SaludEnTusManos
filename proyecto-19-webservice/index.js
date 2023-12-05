a858a4c9b6a964c00f592b36ec2fabc554790fbe
Vanedrn03 <00025822@uca.edu.sv>
2023-12-05 08:32:54 -0600

    vercel

--- proyecto-19-webservice/index.js
+++ proyecto-19-webservice/index.js
@@ -11,7 +11,12 @@ const { errorHandler } = require("./src/middleware/error.middleware");
 
 //const CLIENT_URL = process.env.CLIENT_URL;
 const app = express();
-
+const corsOptions = {
+    origin: 'https://salud-en-tus-manos.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}
+console.log(corsOptions);
 database.connect();
 
 app.use(logger("dev"));
@@ -19,11 +24,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-    origin: 'https://salud-en-tus-manos.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}))
+app.use(cors(corsOptions));
 
 //Static routes
 app.use(express.static(path.join(__dirname, "public")));
