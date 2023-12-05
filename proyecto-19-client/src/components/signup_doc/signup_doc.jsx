fc3f0f79d1b4b4a5ae9a349e7e899b2e882f6ffe
FabrizioCalderon <00034022@uca.edu.sv>
2023-12-05 00:36:48 -0600

    usenavigate

--- proyecto-19-client/src/components/signup_doc/signup_doc.jsx
+++ proyecto-19-client/src/components/signup_doc/signup_doc.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import SignUpimgdoc from "../../images/doc.png";
 import './signUp_Doc.css';
+import { Link, useNavigate } from 'react-router-dom';  
 
 const SignUpDoc = () => {
 
