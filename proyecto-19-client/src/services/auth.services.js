936bb3affb0ec481a646e395b82c45b62796f8dc
FabrizioCalderon <00034022@uca.edu.sv>
2023-12-05 01:59:36 -0600

    prueba

--- proyecto-19-client/src/services/auth.services.js
+++ proyecto-19-client/src/services/auth.services.js
@@ -37,37 +37,3 @@ export const registerUser = async (formData) => {
 };
 
 
-
-export const loginDoctor = async (formData) => {
-  try {
-    const response = await axios.post("/doctores/login", formData);
-
-    if (response.status === 200) {
-      return [200, response.data];
-    }
-
-    if (response.status === 404) {
-      return [404, response.status];
-    }
-  } catch (error) {
-    return 404;
-  }
-};
-
-export const registerDoctor = async (formData) => {
-  try {
-    const response = await axios.post("/doctores/register", formData);
-
-    if (response.status === 201) {
-      return [201, response.data];
-    }
-  } catch (error) {
-    if (error.response.status === 409) {
-      return [409, error.response.data.error];
-    }
-
-    if (error.response.status === 400) {
-      return [400, error.response.data.errors];
-    }
-  }
-};
