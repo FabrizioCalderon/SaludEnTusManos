bf098a9fbe4a4d4ea8da9f53a7657b184a189c22
Luis Mario Cabrera Torres <104865952+MarioCab07@users.noreply.github.com>
2023-12-04 19:21:50 -0600

    Update validator.middleware.js

--- proyecto-19-webservice/src/middleware/validator.middleware.js
+++ proyecto-19-webservice/src/middleware/validator.middleware.js
@@ -1,17 +1,60 @@
-const { validationResult } = require('express-validator');
-const debug = require("debug")("clinica-api:validator");
+const debug = require("debug")("app:auth-middleware");
+const { verifyToken } = require("../utils/jwt.tools");
 
-const runValidation = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        debug(errors);
-        return res.status(400).json({
-            errors: errors.array().map(item => item.msg)
-        });
+const User = require("../models/user.model");
+
+const middleware = {};
+
+const PREFIX = "Bearer";
+
+middleware.authentication = async (req, res, next) => {
+  try {
+    debug("User authentication");
+
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res.status(401).json({ error: "User not authenticated" });
     }
 
-    next();
+    const [prefix, token] = authorization.split(" ");
 
+    if (prefix !== PREFIX) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+
+    if (!token) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+
+    const payload = await verifyToken(token);
+
+    if (!payload) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+
+    const userId = payload["sub"];
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+
+    const isTokenValid = user.tokens.includes(token);
+
+    if (!isTokenValid) {
+      return res.status(401).json({ error: "User not authenticated" });
+    }
+
+    req.user = user;
+    req.token = token;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-module.exports = { runValidation };
+
+module.exports = middleware;
