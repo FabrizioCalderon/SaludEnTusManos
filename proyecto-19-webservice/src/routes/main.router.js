66b0963d01710989bd4105774c7040c11314d5be
Vanedrn03 <00025822@uca.edu.sv>
2023-12-05 04:29:21 -0600

    fixing bugx2

--- proyecto-19-webservice/src/routes/main.router.js
+++ proyecto-19-webservice/src/routes/main.router.js
@@ -3,12 +3,6 @@ const router = express.Router();
 
 const citasRouter = require("./cita.router");
 const authRouter = require("./auth.router");
-<<<<<<< HEAD
-
-
-router.use("/citas", citasRouter);
-router.use("/auth", authRouter);
-=======
 const doctorRouter = require("./doctor.router");
 const antecedentesRouter = require("./antecedentes.router");
 
@@ -16,6 +10,5 @@ router.use("/citas", citasRouter);
 router.use("/auth", authRouter);
 router.use("/doctor", doctorRouter);
 router.use("/antecedentes", antecedentesRouter);
->>>>>>> 78c2c10f (antecedentes)
 
 module.exports = router;
