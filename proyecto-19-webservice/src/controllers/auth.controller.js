958dab65a2b268995b18af215da33e8668b66a06
Luis Mario Cabrera Torres <104865952+MarioCab07@users.noreply.github.com>
2023-12-04 19:13:39 -0600

    Update auth.controller.js

--- proyecto-19-webservice/src/controllers/auth.controller.js
+++ proyecto-19-webservice/src/controllers/auth.controller.js
@@ -1,16 +1,74 @@
 // controllers/user.controller.js
-const RegisterUser = require('../models/user.model');
+const User = require('../models/user.model');
+const { createToken, verifyToken } = require("../utils/jwt.tools");
+const controller = {};
 
-const registerUser = async (req, res) => {
+
+controller.register = async (req, res, next) => {
   try {
-    const newRegisterUser = new RegisterUser(req.body);
-    const savedUser = await newRegisterUser.save();
-    res.json(savedUser);
+    const { username, email, password } = req.body;
+    const user = await User.findOne({
+      $or: [{ username: username }, { email: email }],
+    });
+
+    if (user) {
+      return res.status(409).json({ error: "User already exists" });
+    }
+
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: password,
+      roles: [ROLES.USER],
+    });
+
+    await newUser.save();
+
+    return res.status(201).json({ message: "User registered" });
   } catch (error) {
-    res.status(500).json({ error: 'Error al registrar usuario' });
+    next(error);
   }
 };
 
-module.exports = {
-  registerUser,
+controller.login = async (req, res, next) => {
+  try {
+    const { identifier, password } = req.body;
+
+    const user = await User.findOne({
+      $or: [{ username: identifier }, { email: identifier }],
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (!user.comparedPassword(password)) {
+      return res.status(401).json({ error: "Incorrect Password" });
+    }
+
+    const token = await createToken(user._id);
+
+    let _tokens = [...user.tokens];
+
+    const _verifyPromises = _tokens.map(async (_t) => {
+      const status = await verifyToken(_t);
+      return status ? _t : null;
+    });
+
+    _tokens = (await Promise.all(_verifyPromises))
+      .filter((_t) => _t)
+      .slice(0, 4);
+
+    _tokens = [token, ..._tokens];
+    user.tokens = _tokens;
+
+    await user.save();
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
 };
+
+
+module.exports= controller;
