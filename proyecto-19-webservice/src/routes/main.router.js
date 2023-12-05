8740a5796b2a24c9f929151b19ee00644817641a
FabrizioCalderon <00034022@uca.edu.sv>
2023-12-05 01:25:29 -0600

    doc

--- proyecto-19-webservice/src/routes/main.router.js
+++ proyecto-19-webservice/src/routes/main.router.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const citasRouter = require("./cita.router");
 const authRouter = require("./auth.router");
+const docRouter = require("./doc.router");
 
 router.use("/citas", citasRouter);
 router.use("/auth", authRouter);
+router.use("/doctores", docRouter);
 
 module.exports = router;
