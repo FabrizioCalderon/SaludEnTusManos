74b3930413bc82250071d5de79ea06eb5d908dff
FabrizioCalderon <00034022@uca.edu.sv>
2023-12-05 00:34:58 -0600

    sign

--- proyecto-19-client/src/components/signup_doc/signup_doc.jsx
+++ proyecto-19-client/src/components/signup_doc/signup_doc.jsx
@@ -51,47 +51,66 @@ const SignUpDoc = () => {
     return (
         <div className="signup-container">
       
-        <div className="form-container">
-         
-          <form className="signup-form" onSubmit={handleSubmit} >
-            <div className="form-group">
-              <input
-                type="text"
-                placeholder="Nombres"
-                name='nombre'
-                value={formData.nombre}
-                onChange={handleChange}
-                required
-              />
-              <input
-                type="text"
-                placeholder="Apellidos"
-                name='apellido'
-                value={formData.apellido}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="text"
-                placeholder="DUI (Número de Documento)"
-                name='dui'
-                value={formData.dui}
-                onChange={handleChange}
-                required
-              />
-              <input
-                type="text"
-                placeholder="Credencial Médica"
-                name='credencial'
-                value={formData.credencial}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
+      <div className="form-container">
+       
+        <form className="signup-form" onSubmit={handleSubmit} >
+          <div className="form-group">
             <input
+              type="text"
+              placeholder="Nombres"
+              name='nombre'
+              value={formData.nombre}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Apellidos"
+              name='apellido'
+              value={formData.apellido}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="DUI (Número de Documento)"
+              name='dui'
+              value={formData.dui}
+              onChange={handleChange}
+              required
+            />
+            <select
+              value={formData.sexo}
+              name='sexo'
+              onChange={handleChange}
+              required
+            >
+              <option value="">Sexo</option>
+              <option value="masculino">Masculino</option>
+              <option value="femenino">Femenino</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <input
+              type="date"
+              placeholder="Fecha de Nacimiento"
+              name='fechaNacimiento'
+              value={formData.fechaNacimiento}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Tipo de Sangre"
+              name='tipoSangre'
+              value={formData.tipoSangre}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <input
             type="email"
             placeholder="E-mail"
             name='email'
@@ -99,7 +118,7 @@ const SignUpDoc = () => {
             onChange={handleChange}
             required
           />
-             <input
+          <input
             type="tel"
             placeholder="Número de Teléfono"
             name='numeroTelefono'
@@ -107,32 +126,30 @@ const SignUpDoc = () => {
             onChange={handleChange}
             required
           />
-            </div>
-           
-           
-           <div className="form-group">
-    <input
-      type="password"
-      name='password'
-      placeholder="Contraseña"
-      value={formData.password}
-      onChange={handleChange}
-      required
-    />
-  </div>
-  
-  <div className="form-group">
-    <input
-      type="password"  // Corrige el tipo a 'password'
-      name='confirmPassword'  // Mantiene el nombre como 'confirmPassword'
-      placeholder="Confirmar Contraseña"
-      value={formData.confirmPassword}
-      onChange={handleChange}
-      required
-    />
-  </div>
-                
+         <div className="form-group">
+  <input
+    type="password"
+    name='password'
+    placeholder="Contraseña"
+    value={formData.password}
+    onChange={handleChange}
+    required
+  />
+</div>
+
+<div className="form-group">
+  <input
+    type="password"  // Corrige el tipo a 'password'
+    name='confirmPassword'  // Mantiene el nombre como 'confirmPassword'
+    placeholder="Confirmar Contraseña"
+    value={formData.confirmPassword}
+    onChange={handleChange}
+    required
+  />
+</div>
+                    
                     <button type="submit">Registrarse</button>
+                   
                     {/* Agrega un enlace para ir a la página de inicio de sesión */}
                     <p className="login-link">
                         ¿Ya tienes una cuenta? <Link to="/logindoc">Inicia sesión aquí</Link>
